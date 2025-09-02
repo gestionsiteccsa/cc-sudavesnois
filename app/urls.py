2abcab285229441c5f4df1795b44abd7d088eeac
User <user@example.com>
2025-09-02 15:06:47 +0200

    fix ulrs.py

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
 from django.views.generic import RedirectView, TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 from home.sitemaps import CommunesSitemap, JournalSitemap, StaticViewSitemap
 
@@ -59,3 +61,6 @@ urlpatterns = [
         name="profile_redirect",
     ),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
