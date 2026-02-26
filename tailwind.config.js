6e44b901eb30170eb801658559c36b7d6ca93e43
Developer <developer@example.com>
2026-02-26 16:45:43 +0100

    Add YouTube and Instagram social media icons to header and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,11 @@ module.exports = {
     extend: {
       colors: {
         primary: '#006ab3',
-        secondary: '#96bf0d'
+        secondary: '#96bf0d',
+        facebook: '#1877F2',
+        linkedin: '#0A66C2',
+        youtube: '#FF0000',
+        instagram: '#E4405F'
       },
       fontFamily: {
         sans: ['Roboto', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Arial', 'sans-serif'],
