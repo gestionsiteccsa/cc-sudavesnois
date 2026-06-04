44f32904d25c433ffb781d0c9d29dee371a86477
Développeur <dev@ccsudavesnois.fr>
2026-06-04 10:45:05 +0200

    fix(search): corrige l'URL des resultats de recherche pour les communes

--- conseil_communautaire/apps.py
+++ conseil_communautaire/apps.py
@@ -1,17 +1,21 @@
 from django.apps import AppConfig
+
 from watson import search as watson
 
 
 class ConseilCommunautaireConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "conseil_communautaire"
-    
+
     def ready(self):
-        from .models import ConseilVille
         from django.urls import reverse
-        
+
+        from .models import ConseilVille
+
         class ConseilVilleAdapter(watson.SearchAdapter):
             def get_url(self, obj):
-                return reverse('conseil_communautaire:conseil')
-        
-        watson.register(ConseilVille, ConseilVilleAdapter, fields=("city_name", "slogan"))
+                return reverse("communes-membres:commune", kwargs={"slug": obj.slug})
+
+        watson.register(
+            ConseilVille, ConseilVilleAdapter, fields=("city_name", "slogan")
+        )
